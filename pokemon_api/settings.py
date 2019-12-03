50b579f5d6b3b1960a679fbf420ec034482a84b8
Abhinav Masalia <abhinav.masalia99@gmail.com>
2019-12-04 03:49:07 +0530

    Set up the Django REST Framework
    Also, serialized the model that we created earlier.

--- pokemon_api/settings.py
+++ pokemon_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'pokemon_app.apps.PokemonAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ pokemon_app/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Hero
+
+class PokemonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Pokemon
+        fields = ('name', 'type')
