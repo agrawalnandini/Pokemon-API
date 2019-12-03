94b179ec894904fafdd6598c4cefc4d23ab331e7
Abhinav Masalia <abhinav.masalia99@gmail.com>
2019-12-04 02:26:46 +0530

    Created a super user for admin privileges

--- pokemon_api/settings.py
+++ pokemon_api/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pokemon_app.apps.PokemonAppConfig'
+    'pokemon_app.apps.PokemonAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
