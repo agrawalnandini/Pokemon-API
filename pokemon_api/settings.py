9f96e4286fb20e22bb779c19dd30111a1a5e4b90
Abhinav Masalia <abhinav.masalia99@gmail.com>
2019-12-04 01:37:07 +0530

    Added the newly created 'pokemon_app' to settings.py

--- pokemon_api/settings.py
+++ pokemon_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pokemon_app.apps.PokemonAppConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ pokemon_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PokemonAppConfig(AppConfig):
+    name = 'pokemon_app'
