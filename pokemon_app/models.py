a6e39f0d97af94cc2f46ecff1045410a63587cc1
Nandini Agrawal <nandiniagrawal@Nandinis-MacBook-Air-2.local>
2019-12-04 19:37:20 +0530

    Added in models

--- pokemon_app/models.py
+++ pokemon_app/models.py
@@ -3,5 +3,16 @@ from django.db import models
 class Pokemon(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
+    type2= models.CharField(max_length=50)
+    total=models.IntegerField()
+    hp=models.IntegerField()
+    attack=models.IntegerField()
+    defense=models.IntegerField()
+    sp_atk=models.IntegerField()
+    sp_def=models.IntegerField()
+    speed=models.IntegerField()
+    generation=models.IntegerField()
+    legend=models.BooleanField()
+
     def __str__(self):
         return self.name
