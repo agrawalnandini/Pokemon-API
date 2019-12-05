1a76f921118c3ba3703b76e8790e78db682cd047
Abhinav Masalia <abhinav.masalia99@gmail.com>
2019-12-05 17:45:48 +0530

    Populated the database with the pokemon data
    Changed serializer to accept lists and dictionaries, so that multiple instances of the model can be added when using POST.

--- pokemon_app/views.py
+++ pokemon_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .serializers import PokemonSerializer
 from .models import Pokemon
 
 
 class PokemonViewSet(viewsets.ModelViewSet):
-    queryset = Pokemon.objects.all().order_by('name')
+    queryset = Pokemon.objects.all().order_by('serial')
     serializer_class = PokemonSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
