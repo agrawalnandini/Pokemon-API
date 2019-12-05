330858a12982ee967c7076d3b94dcbc7e62afec5
Abhinav Masalia <abhinav.masalia99@gmail.com>
2019-12-05 13:20:41 +0530

    The database queryset now passes into the serializer to get rendered into JSON format

--- pokemon_app/views.py
+++ pokemon_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import PokemonSerializer
+from .models import Pokemon
+
+
+class PokemonViewSet(viewsets.ModelViewSet):
+    queryset = Pokemon.objects.all().order_by('name')
+    serializer_class = PokemonSerializer
