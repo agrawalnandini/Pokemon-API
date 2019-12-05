f8cae2a0a37e6682ff43ab16320450ed356e7eff
Abhinav Masalia <abhinav.masalia99@gmail.com>
2019-12-05 17:16:48 +0530

    Made some minor changes in nomenclature and format of database

--- pokemon_app/models.py
+++ pokemon_app/models.py
@@ -13,7 +13,7 @@ class Pokemon(models.Model):
     sp_def = models.IntegerField(null = True)
     speed = models.IntegerField(null = True)
     generation = models.IntegerField(null = True)
-    legend = models.BooleanField(null = True)
+    legendary = models.BooleanField(null = True)
 
     def __str__(self):
         return self.name
