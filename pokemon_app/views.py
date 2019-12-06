085c004dd65fed72d1d5f4d773968e0e8b49eab2
Abhinav Masalia <abhinav.masalia99@gmail.com>
2019-12-06 13:12:17 +0530

    Ordering filter added for the rendered dataset.

--- pokemon_app/views.py
+++ pokemon_app/views.py
@@ -11,8 +11,10 @@ from .models import Pokemon
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all().order_by('serial')
     serializer_class = PokemonSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['name', 'type1', 'type2']
+    ordering_fields = ['serial', 'name', 'hp', 'attack', 'defense', 'sp_atk', 'sp_def', 'speed', 'generation']
+    ordering = ['serial']
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
